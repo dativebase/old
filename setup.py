e432d380d7882bd8480b24c1fe5445c94747837d
jrwdunham <jrwdunham@gmail.com>
2012-11-22 18:10:58 -0800

    Updated help/git_help with info about branching & mergin; fixed SQLAlchemy requirement in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url='',
     install_requires=[
         "Pylons>=1.0,<=1.0.99",
-        "SQLAlchemy>=0.5,<0.7.9",
+        "SQLAlchemy>=0.5,<=0.7.9",
         "WebOb<=1.0"
     ],
     setup_requires=["PasteScript>=1.6.3"],
