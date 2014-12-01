c56a431bd767ff4da1b3fe6244989daa2c17bdb3
Joel Dunham <jrwdunham@gmail.com>
2014-11-30 21:54:03 -0800

    Added CORS-specific headers to `config/middleware.py`.

--- onlinelinguisticdatabase/config/middleware.py
+++ onlinelinguisticdatabase/config/middleware.py
@@ -28,6 +28,7 @@ from pylons.wsgiapp import PylonsApp
 from routes.middleware import RoutesMiddleware
 from onlinelinguisticdatabase.config.environment import load_environment
 import logging
+import pprint
 
 log = logging.getLogger(__name__)
 
@@ -40,10 +41,52 @@ class HTML2JSONContentType(object):
 
     def __call__(self, environ, start_response):
         def custom_start_response(status, headers, exc_info=None):
+
+            new_headers = dict(headers)
+
             if dict(headers).get('Content-Type') == 'text/html; charset=utf-8':
-                new_headers = dict(headers)
                 new_headers['Content-Type'] = 'application/json'
-                headers = new_headers.items()
+
+            # CORS stuff. See http://stackoverflow.com/questions/2771974/modify-headers-in-pylons-using-middleware
+
+            # Use '*' to allow all origins
+            new_headers['Access-Control-Allow-Origin'] = 'http://localhost:9000'
+
+            # Use this header to indicate that cookies should be included in CORS requests.
+            new_headers['Access-Control-Allow-Credentials'] = 'true'
+
+            # What was here before: new_headers['Access-Control-Allow-Methods'] = 'OPTIONS, GET, POST'
+            new_headers['Access-Control-Allow-Methods'] = 'GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT, COPY, OPTIONS'
+
+            # What was here before: new_headers['Access-Control-Allow-Headers'] = 'Content-Type, content-type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control'
+            new_headers['Access-Control-Allow-Headers'] = 'Content-Type, content-type'
+
+            # This causes the preflight result to be cached for specified milliseconds.
+            # From LingSync's CouchDB config
+            # NOTE: Comment out during development
+            #new_headers['Access-Control-Max-Age'] = '12345'
+
+            # Access-Control-Expose-Headers (optional)
+            # The XMLHttpRequest 2 object has a getResponseHeader() method that returns the
+            # value of a particular response header. During a CORS request, the
+            # getResponseHeader() method can only access simple response headers. Simple
+            # response headers are defined as follows:
+            #
+            #    Cache-Control
+            #    Content-Language
+            #    Content-Type
+            #    Expires
+            #    Last-Modified
+            #    Pragma
+            #
+            # If you want clients to be able to access other headers, you have to use
+            # the Access-Control-Expose-Headers header. The value of this header is a
+            # comma-delimited list of response headers you want to expose to the client.
+            # NOTE: Commented this out for debuggin ...
+            new_headers['Access-Control-Expose-Headers'] = 'Access-Control-Allow-Origin, Access-Control-Allow-Credentials'
+
+            headers = new_headers.items()
+
             return start_response(status, headers, exc_info)
 
         return self.app(environ, custom_start_response)
